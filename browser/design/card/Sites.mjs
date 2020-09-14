b54f66858faf2828ed676aef47762704b319ac6f
Cookie Engineer <cookiengineer@protonmail.ch>
2020-09-14 14:28:43 +0200

    :rocket: Sites Search for Settings

--- browser/design/card/Sites.mjs
+++ browser/design/card/Sites.mjs
@@ -1,14 +1,79 @@
 
 import { Element                     } from '../Element.mjs';
 import { Widget                      } from '../Widget.mjs';
+import { Beacon                      } from '../card/Beacon.mjs';
+import { Host                        } from '../card/Host.mjs';
+import { Mode                        } from '../card/Mode.mjs';
+import { Peer                        } from '../card/Peer.mjs';
+import { Redirect                    } from '../card/Redirect.mjs';
 import { isArray, isObject, isString } from '../../extern/base.mjs';
 
 
 
 const update = function(browser) {
 
-	// TODO: The value() method should do this when {results} is set!
-	// TODO: Refresh this.results based on browser.settings{} and domain value
+	let value = this.value();
+
+	if (isObject(value) === true && isString(value.domain) === true) {
+
+		this.results = {};
+
+
+		let domain = value.domain;
+		let prefix = null;
+		let midfix = null;
+		let suffix = null;
+
+
+		if (domain.includes('*')) {
+
+			if (domain.startsWith('*')) {
+				suffix = domain.split('*').pop();
+			}
+
+			if (domain.endsWith('*')) {
+				prefix = domain.split('*').shift();
+			}
+
+		} else {
+
+			midfix = domain;
+
+		}
+
+
+		this.allowed.sort().forEach((type) => {
+
+			browser.settings[type].forEach((entry) => {
+
+				if (
+					(prefix === null || entry.domain.startsWith(prefix))
+					&& (midfix === null || entry.domain.includes(midfix))
+					&& (suffix === null || entry.domain.endsWith(suffix))
+				) {
+
+					if (isObject(this.results[entry.domain]) === false) {
+
+						this.results[entry.domain] = {
+							beacons:   [],
+							hosts:     [],
+							modes:     [],
+							peers:     [],
+							redirects: [],
+							sessions:  []
+						};
+
+					}
+
+					this.results[entry.domain][type].push(entry);
+
+				}
+
+			});
+
+		});
+
+	}
 
 
 	let element = this.element.query('[data-key="results"]');
@@ -16,20 +81,149 @@ const update = function(browser) {
 
 		element.value(this.allowed.sort().map((type) => {
 
-			let visible = this.results[type].length;
+			let visible = Object.values(this.results).map((domain) => domain[type].length).reduce((a, b) => a + b, 0);
 			let overall = browser.settings[type].length;
 
-			return visible + ' of ' + overall + ' ' + (type.charAt(0).toUpperCase() + type.substr(1)) + (visible > 1 ? 's' : '');
+			return visible + ' of ' + overall + ' ' + type.charAt(0).toUpperCase() + type.substr(1);
 
 		}).join(', '));
 
 	}
 
-	let total = Object.values(this.results).reduce((a, b) => a + b, 0);
-	if (total > 1) {
-		this.buttons.remove.state('enabled');
-	} else {
-		this.buttons.remove.state('disabled');
+
+	let article = this.element.query('browser-card-sites-article');
+	if (article !== null) {
+
+		article.query('*', true).forEach((element) => {
+			element.erase();
+		});
+
+		Object.keys(this.results).sort().forEach((domain) => {
+
+			let cards = [];
+			let entry = this.results[domain];
+
+			if (this.actions.includes('remove')) {
+
+				let headline = new Element('h4', [
+					domain,
+					'<button title="Remove ' + domain + '" data-action="remove"></button>'
+				]);
+
+				let button = headline.query('[data-action="remove"]');
+				if (button !== null) {
+
+					button.on('click', () => {
+
+						cards.forEach((card) => {
+
+							let remove = card.query('[data-action="remove"]');
+							if (remove !== null) {
+								remove.emit('click');
+							}
+
+							setTimeout(() => {
+								card.erase();
+							}, 0);
+
+						});
+
+						setTimeout(() => {
+							headline.erase();
+						}, 0);
+
+					});
+
+				}
+
+				headline.render(article);
+
+			} else {
+
+				new Element('h4', [ domain ]).render(article);
+
+			}
+
+			if (entry.beacons.length > 0) {
+
+				entry.beacons.map((beacon) => {
+
+					if (this.actions.includes('remove')) {
+						return Beacon.from(beacon, [ 'remove', 'save' ]);
+					} else {
+						return Beacon.from(beacon, [ 'save' ]);
+					}
+
+				}).forEach((card) => cards.push(card));
+
+			}
+
+			if (entry.hosts.length > 0) {
+
+				entry.hosts.map((host) => {
+
+					if (this.actions.includes('remove')) {
+						return Host.from(host, [ 'remove', 'save' ]);
+					} else {
+						return Host.from(host, [ 'save' ]);
+					}
+
+				}).forEach((card) => cards.push(card));
+
+			}
+
+			if (entry.modes.length > 0) {
+
+				entry.modes.map((mode) => {
+
+					if (this.actions.includes('remove')) {
+						return Mode.from(mode, [ 'remove', 'save' ]);
+					} else {
+						return Mode.from(mode, [ 'save' ]);
+					}
+
+				}).forEach((card) => cards.push(card));
+
+			}
+
+			if (entry.peers.length > 0) {
+
+				entry.peers.map((peer) => {
+
+					if (this.actions.includes('remove')) {
+						return Peer.from(peer, [ 'remove', 'save' ]);
+					} else {
+						return Peer.from(peer, [ 'save' ]);
+					}
+
+				}).forEach((card) => cards.push(card));
+
+			}
+
+			if (entry.redirects.length > 0) {
+
+				entry.redirects.map((redirect) => {
+
+					if (this.actions.includes('remove')) {
+						return Redirect.from(redirect, [ 'remove', 'save' ]);
+					} else {
+						return Redirect.from(redirect, [ 'save' ]);
+					}
+
+				}).forEach((card) => cards.push(card));
+
+			}
+
+			if (cards.length > 0) {
+
+				cards.forEach((card) => {
+					card.render(article);
+				});
+
+			}
+
+		});
+
 	}
 
 };
@@ -38,8 +232,8 @@ const update = function(browser) {
 
 const Sites = function(browser, allowed, actions) {
 
-	this.actions = isArray(actions) ? actions : [ 'refresh', 'remove' ];
 	this.allowed = isArray(allowed) ? allowed : [ 'beacons', 'hosts', 'modes', 'peers', 'redirects', 'sessions' ];
+	this.actions = isArray(actions) ? actions : [ 'refresh', 'remove' ];
 	this.element = new Element('browser-card-sites', [
 		'<h3>Sites</h3>',
 		'<button title="Toggle visibility of this Card" data-action="toggle"></button>',
@@ -50,17 +244,14 @@ const Sites = function(browser, allowed, actions) {
 		'<p data-key="results">0 of 0 Beacons, 0 of 0 Hosts, 0 of 0 Modes, 0 of 0 Peers, 0 of 0 Redirects, 0 of 0 Sessions</p>',
 		'</browser-card-sites-header>',
 		'<browser-card-sites-article>',
-
 		'</browser-card-sites-article>',
 		'<browser-card-sites-footer>',
 		'<button title="Refresh all results" data-action="refresh"></button>',
-		'<button title="Remove all results" data-action="remove"></button>',
 		'</browser-card-sites-footer>'
 	]);
 
 	this.buttons = {
 		refresh: this.element.query('button[data-action="refresh"]'),
-		remove:  this.element.query('button[data-action="remove"]'),
 		toggle:  this.element.query('button[data-action="toggle"]')
 	};
 
@@ -68,16 +259,10 @@ const Sites = function(browser, allowed, actions) {
 		domain: this.element.query('[data-key="domain"]')
 	};
 
-	this.results = {
-		beacons:   [],
-		hosts:     [],
-		modes:     [],
-		peers:     [],
-		redirects: [],
-		sessions:  []
-	};
+	this.results = {};
 
 	Widget.call(this);
+
 
 	window._SITES = this;
 
@@ -119,7 +304,6 @@ const Sites = function(browser, allowed, actions) {
 	this.element.on('update', () => {
 
 		this.buttons.refresh.erase();
-		this.buttons.remove.erase();
 
 
 		this.model.domain.state('enabled');
@@ -131,10 +315,6 @@ const Sites = function(browser, allowed, actions) {
 			this.buttons.refresh.render(footer);
 		}
 
-		if (this.actions.includes('remove')) {
-			this.buttons.remove.render(footer);
-		}
-
 		update.call(this, browser);
 
 	});
@@ -143,7 +323,11 @@ const Sites = function(browser, allowed, actions) {
 
 		this.buttons.refresh.on('click', () => {
 
-			console.log('SEARCH NAO!');
+			this.model.domain.validate();
+
+			if (this.model.domain.state() === 'enabled') {
+				update.call(this, browser);
+			}
 
 		});
 
@@ -170,10 +354,10 @@ const Sites = function(browser, allowed, actions) {
 };
 
 
-Sites.from = function(value, results, actions) {
+Sites.from = function(value, allowed, actions) {
 
 	value   = isObject(value)  ? value   : null;
-	results = isArray(results) ? results : null;
+	allowed = isArray(allowed) ? allowed : null;
 	actions = isArray(actions) ? actions : null;
 
 
@@ -181,7 +365,7 @@ Sites.from = function(value, results, actions) {
 
 	if (value !== null) {
 
-		widget = new Sites(window.parent.BROWSER || null, results, actions);
+		widget = new Sites(window.parent.BROWSER || null, allowed, actions);
 		widget.value(value);
 
 	}
