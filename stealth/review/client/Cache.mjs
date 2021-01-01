c1bdaa0869648d36eb432cbc57a19b2162fa92b7
Cookie Engineer <cookiengineer@protonmail.ch>
2021-01-01 15:27:15 +0100

    :rocket: Better Cache Review

--- stealth/review/client/Cache.mjs
+++ stealth/review/client/Cache.mjs
@@ -2,6 +2,7 @@
 import { isBuffer, isFunction, isNumber, isObject, isString           } from '../../../base/index.mjs';
 import { after, before, describe, finish                              } from '../../../covert/index.mjs';
 import { Cache                                                        } from '../../../stealth/source/client/Cache.mjs';
+import { DATETIME                                                     } from '../../../stealth/source/parser/DATETIME.mjs';
 import { connect as connect_stealth, disconnect as disconnect_stealth } from '../Stealth.mjs';
 import { connect as connect_client, disconnect as disconnect_client   } from '../Client.mjs';
 
@@ -32,14 +33,14 @@ describe('Cache.prototype.toJSON()', function(assert) {
 
 });
 
-describe('Cache.prototype.save()', function(assert) {
+describe('Cache.prototype.save()/1', function(assert) {
 
 	assert(this.client !== null);
 	assert(isFunction(this.client.services.cache.save), true);
 
 	this.client.services.cache.save({
 		domain: 'example.com',
-		path:   '/review/client/cache.json',
+		path:   '/review/client/cache.json?foo=bar',
 		headers: {
 			'x-test': 'save'
 		},
@@ -54,71 +55,20 @@ describe('Cache.prototype.save()', function(assert) {
 
 });
 
-describe('Cache.prototype.info()', function(assert) {
+describe('Cache.prototype.save()/2', function(assert) {
 
 	assert(this.client !== null);
-	assert(isFunction(this.client.services.cache.info), true);
+	assert(isFunction(this.client.services.cache.save), true);
 
-	this.client.services.cache.info({
+	this.client.services.cache.save({
 		domain: 'example.com',
-		path:   '/review/client/cache.json'
-	}, (response) => {
-
-		assert(isObject(response), true);
-
-		assert(isObject(response.headers),      true);
-		assert(isNumber(response.headers.size), true);
-		assert(isString(response.headers.time), true);
-
-		assert(isObject(response.payload),      true);
-		assert(isNumber(response.payload.size), true);
-		assert(isString(response.payload.time), true);
-
-	});
-
-});
-
-describe('Cache.prototype.read()/success', function(assert) {
-
-	assert(this.client !== null);
-	assert(isFunction(this.client.services.cache.read), true);
-
-	this.client.services.cache.read({
-		domain: 'example.com',
-		path:   '/review/client/cache.json'
-	}, (response) => {
-
-		assert(isObject(response),         true);
-		assert(isObject(response.headers), true);
-		assert(isBuffer(response.payload), true);
-
-		assert(response.headers, {
-			'x-test':         'save',
-			'content-type':   'application/json',
-			'content-length': 17
-		});
-
-		let data = null;
-		try {
-			data = JSON.parse(response.payload.toString('utf8'));
-		} catch (err) {
-			data = null;
+		path:   '/review/client/cache.json?foo=qux',
+		headers: {
+			'x-test': 'save'
+		},
+		payload: {
+			foo: 'qux doo'
 		}
-
-		assert(data, { foo: 'bar' });
-
-	});
-
-});
-
-describe('Cache.prototype.remove()/success', function(assert) {
-
-	assert(this.client !== null);
-	assert(isFunction(this.client.services.cache.remove), true);
-
-	this.client.services.cache.remove({
-		domain: 'example.com',
-		path:   '/review/client/cache.json'
 	}, (response) => {
 
 		assert(response, true);
@@ -127,7 +77,59 @@ describe('Cache.prototype.remove()/success', function(assert) {
 
 });
 
-describe('Cache.prototype.read()/failure', function(assert) {
+describe('Cache.prototype.info()/1', function(assert) {
+
+	assert(this.client !== null);
+	assert(isFunction(this.client.services.cache.info), true);
+
+	this.client.services.cache.info({
+		domain: 'example.com',
+		path:   '/review/client/cache.json?foo=bar'
+	}, (response) => {
+
+		assert(isObject(response), true);
+
+		assert(isObject(response.headers),             true);
+		assert(isNumber(response.headers.size),        true);
+		assert(DATETIME.isDate(response.headers.date), true);
+		assert(DATETIME.isTime(response.headers.time), true);
+
+		assert(isObject(response.payload),             true);
+		assert(isNumber(response.payload.size),        true);
+		assert(DATETIME.isDate(response.payload.date), true);
+		assert(DATETIME.isTime(response.payload.time), true);
+
+	});
+
+});
+
+describe('Cache.prototype.info()/2', function(assert) {
+
+	assert(this.client !== null);
+	assert(isFunction(this.client.services.cache.info), true);
+
+	this.client.services.cache.info({
+		domain: 'example.com',
+		path:   '/review/client/cache.json?foo=qux'
+	}, (response) => {
+
+		assert(isObject(response), true);
+
+		assert(isObject(response.headers),             true);
+		assert(isNumber(response.headers.size),        true);
+		assert(DATETIME.isDate(response.headers.date), true);
+		assert(DATETIME.isTime(response.headers.time), true);
+
+		assert(isObject(response.payload),             true);
+		assert(isNumber(response.payload.size),        true);
+		assert(DATETIME.isDate(response.payload.date), true);
+		assert(DATETIME.isTime(response.payload.time), true);
+
+	});
+
+});
+
+describe('Cache.prototype.read()/3/failure', function(assert) {
 
 	assert(this.client !== null);
 	assert(isFunction(this.client.services.cache.read), true);
@@ -143,14 +145,78 @@ describe('Cache.prototype.read()/failure', function(assert) {
 
 });
 
-describe('Cache.prototype.remove()/failure', function(assert) {
+describe('Cache.prototype.read()/1/success', function(assert) {
 
-	assert(this.client!== null);
+	assert(this.client !== null);
+	assert(isFunction(this.client.services.cache.read), true);
+
+	this.client.services.cache.read({
+		domain: 'example.com',
+		path:   '/review/client/cache.json?foo=bar'
+	}, (response) => {
+
+		assert(isObject(response),         true);
+		assert(isObject(response.headers), true);
+		assert(isBuffer(response.payload), true);
+
+		assert(response.headers['x-test'],                  'save');
+		assert(response.headers['content-type'],            'application/json');
+		assert(response.headers['content-length'],          17);
+		assert(isString(response.headers['last-modified']), true);
+
+		let data = null;
+		try {
+			data = JSON.parse(response.payload.toString('utf8'));
+		} catch (err) {
+			data = null;
+		}
+
+		assert(data, { foo: 'bar' });
+
+	});
+
+});
+
+describe('Cache.prototype.read()/2/success', function(assert) {
+
+	assert(this.client !== null);
+	assert(isFunction(this.client.services.cache.read), true);
+
+	this.client.services.cache.read({
+		domain: 'example.com',
+		path:   '/review/client/cache.json?foo=qux'
+	}, (response) => {
+
+		assert(isObject(response),         true);
+		assert(isObject(response.headers), true);
+		assert(isBuffer(response.payload), true);
+
+		assert(response.headers['x-test'],                  'save');
+		assert(response.headers['content-type'],            'application/json');
+		assert(response.headers['content-length'],          21);
+		assert(isString(response.headers['last-modified']), true);
+
+		let data = null;
+		try {
+			data = JSON.parse(response.payload.toString('utf8'));
+		} catch (err) {
+			data = null;
+		}
+
+		assert(data, { foo: 'qux doo' });
+
+	});
+
+});
+
+describe('Cache.prototype.remove()/1/success', function(assert) {
+
+	assert(this.client !== null);
 	assert(isFunction(this.client.services.cache.remove), true);
 
 	this.client.services.cache.remove({
 		domain: 'example.com',
-		path:   '/review/client/cache.json'
+		path:   '/review/client/cache.json?foo=bar'
 	}, (response) => {
 
 		assert(response, true);
@@ -158,6 +224,97 @@ describe('Cache.prototype.remove()/failure', function(assert) {
 	});
 
 });
+
+describe('Cache.prototype.read()/1/failure', function(assert) {
+
+	assert(this.client !== null);
+	assert(isFunction(this.client.services.cache.read), true);
+
+	this.client.services.cache.read({
+		domain: 'example.com',
+		path:   '/review/client/cache.json?foo=bar'
+	}, (response) => {
+
+		assert(response, null);
+
+	});
+
+});
+
+describe('Cache.prototype.remove()/1/failure', function(assert) {
+
+	assert(this.client !== null);
+	assert(isFunction(this.client.services.cache.remove), true);
+
+	this.client.services.cache.remove({
+		domain: 'example.com',
+		path:   '/review/client/cache.json?foo=bar'
+	}, (response) => {
+
+		assert(response, false);
+
+	});
+
+});
+
+describe('Cache.prototype.info()/2', function(assert) {
+
+	assert(this.client !== null);
+	assert(isFunction(this.client.services.cache.info), true);
+
+	this.client.services.cache.info({
+		domain: 'example.com',
+		path:   '/review/client/cache.json?foo=qux'
+	}, (response) => {
+
+		assert(isObject(response), true);
+
+		assert(isObject(response.headers),             true);
+		assert(isNumber(response.headers.size),        true);
+		assert(DATETIME.isDate(response.headers.date), true);
+		assert(DATETIME.isTime(response.headers.time), true);
+
+		assert(isObject(response.payload),             true);
+		assert(isNumber(response.payload.size),        true);
+		assert(DATETIME.isDate(response.payload.date), true);
+		assert(DATETIME.isTime(response.payload.time), true);
+
+	});
+
+});
+
+describe('Cache.prototype.remove()/2/success', function(assert) {
+
+	assert(this.client !== null);
+	assert(isFunction(this.client.services.cache.remove), true);
+
+	this.client.services.cache.remove({
+		domain: 'example.com',
+		path:   '/review/client/cache.json?foo=qux'
+	}, (response) => {
+
+		assert(response, true);
+
+	});
+
+});
+
+describe('Cache.prototype.remove()/2/failure', function(assert) {
+
+	assert(this.client !== null);
+	assert(isFunction(this.client.services.cache.remove), true);
+
+	this.client.services.cache.remove({
+		domain: 'example.com',
+		path:   '/review/client/cache.json?foo=qux'
+	}, (response) => {
+
+		assert(response, false);
+
+	});
+
+});
+
 
 after(disconnect_client);
 after(disconnect_stealth);
--- stealth/review/server/Cache.mjs
+++ stealth/review/server/Cache.mjs
@@ -31,14 +31,14 @@ describe('Cache.prototype.toJSON()', function(assert) {
 
 });
 
-describe('Cache.prototype.save()', function(assert) {
+describe('Cache.prototype.save()/1', function(assert) {
 
 	assert(this.server !== null);
 	assert(isFunction(this.server.services.cache.save), true);
 
 	this.server.services.cache.save({
 		domain: 'example.com',
-		path:   '/review/server/cache.json',
+		path:   '/review/server/cache.json?foo=bar',
 		headers: {
 			'x-test': 'save'
 		},
@@ -59,14 +59,42 @@ describe('Cache.prototype.save()', function(assert) {
 
 });
 
-describe('Cache.prototype.info()', function(assert) {
+describe('Cache.prototype.save()/2', function(assert) {
+
+	assert(this.server !== null);
+	assert(isFunction(this.server.services.cache.save), true);
+
+	this.server.services.cache.save({
+		domain: 'example.com',
+		path:   '/review/server/cache.json?foo=qux',
+		headers: {
+			'x-test': 'save'
+		},
+		payload: {
+			foo: 'qux doo'
+		}
+	}, (response) => {
+
+		assert(response, {
+			headers: {
+				service: 'cache',
+				event:   'save'
+			},
+			payload: true
+		});
+
+	});
+
+});
+
+describe('Cache.prototype.info()/1', function(assert) {
 
 	assert(this.server !== null);
 	assert(isFunction(this.server.services.cache.info), true);
 
 	this.server.services.cache.info({
 		domain: 'example.com',
-		path:   '/review/server/cache.json'
+		path:   '/review/server/cache.json?foo=bar'
 	}, (response) => {
 
 		assert(isObject(response), true);
@@ -75,11 +103,13 @@ describe('Cache.prototype.info()', function(assert) {
 			'event':   'info'
 		});
 
-		assert(isObject(response.payload),                     true);
+		assert(isObject(response.payload), true);
+
 		assert(isObject(response.payload.headers),             true);
 		assert(isNumber(response.payload.headers.size),        true);
 		assert(DATETIME.isDate(response.payload.headers.date), true);
 		assert(DATETIME.isTime(response.payload.headers.time), true);
+
 		assert(isObject(response.payload.payload),             true);
 		assert(isNumber(response.payload.payload.size),        true);
 		assert(DATETIME.isDate(response.payload.payload.date), true);
@@ -89,7 +119,39 @@ describe('Cache.prototype.info()', function(assert) {
 
 });
 
-describe('Cache.prototype.read()/success', function(assert) {
+describe('Cache.prototype.info()/2', function(assert) {
+
+	assert(this.server !== null);
+	assert(isFunction(this.server.services.cache.info), true);
+
+	this.server.services.cache.info({
+		domain: 'example.com',
+		path:   '/review/server/cache.json?foo=qux'
+	}, (response) => {
+
+		assert(isObject(response), true);
+		assert(response.headers, {
+			'service': 'cache',
+			'event':   'info'
+		});
+
+		assert(isObject(response.payload), true);
+
+		assert(isObject(response.payload.headers),             true);
+		assert(isNumber(response.payload.headers.size),        true);
+		assert(DATETIME.isDate(response.payload.headers.date), true);
+		assert(DATETIME.isTime(response.payload.headers.time), true);
+
+		assert(isObject(response.payload.payload),             true);
+		assert(isNumber(response.payload.payload.size),        true);
+		assert(DATETIME.isDate(response.payload.payload.date), true);
+		assert(DATETIME.isTime(response.payload.payload.time), true);
+
+	});
+
+});
+
+describe('Cache.prototype.read()/3/failure', function(assert) {
 
 	assert(this.server !== null);
 	assert(isFunction(this.server.services.cache.read), true);
@@ -97,6 +159,28 @@ describe('Cache.prototype.read()/success', function(assert) {
 	this.server.services.cache.read({
 		domain: 'example.com',
 		path:   '/review/server/cache.json'
+	}, (response) => {
+
+		assert(response, {
+			headers: {
+				service: 'cache',
+				event:   'read'
+			},
+			payload: null
+		});
+
+	});
+
+});
+
+describe('Cache.prototype.read()/1/success', function(assert) {
+
+	assert(this.server !== null);
+	assert(isFunction(this.server.services.cache.read), true);
+
+	this.server.services.cache.read({
+		domain: 'example.com',
+		path:   '/review/server/cache.json?foo=bar'
 	}, (response) => {
 
 		assert(isObject(response), true);
@@ -127,14 +211,52 @@ describe('Cache.prototype.read()/success', function(assert) {
 
 });
 
-describe('Cache.prototype.remove()/success', function(assert) {
+describe('Cache.prototype.read()/2/success', function(assert) {
+
+	assert(this.server !== null);
+	assert(isFunction(this.server.services.cache.read), true);
+
+	this.server.services.cache.read({
+		domain: 'example.com',
+		path:   '/review/server/cache.json?foo=qux'
+	}, (response) => {
+
+		assert(isObject(response), true);
+		assert(response.headers, {
+			'service': 'cache',
+			'event':   'read'
+		});
+
+		assert(isObject(response.payload),         true);
+		assert(isObject(response.payload.headers), true);
+		assert(isBuffer(response.payload.payload), true);
+
+		assert(response.payload.headers['x-test'],                  'save');
+		assert(response.payload.headers['content-type'],            'application/json');
+		assert(response.payload.headers['content-length'],          21);
+		assert(isString(response.payload.headers['last-modified']), true);
+
+		let data = null;
+		try {
+			data = JSON.parse(response.payload.payload.toString('utf8'));
+		} catch (err) {
+			data = null;
+		}
+
+		assert(data, { foo: 'qux doo' });
+
+	});
+
+});
+
+describe('Cache.prototype.remove()/1/success', function(assert) {
 
 	assert(this.server !== null);
 	assert(isFunction(this.server.services.cache.remove), true);
 
 	this.server.services.cache.remove({
 		domain: 'example.com',
-		path:   '/review/server/cache.json'
+		path:   '/review/server/cache.json?foo=bar'
 	}, (response) => {
 
 		assert(response, {
@@ -149,14 +271,14 @@ describe('Cache.prototype.remove()/success', function(assert) {
 
 });
 
-describe('Cache.prototype.read()/failure', function(assert) {
+describe('Cache.prototype.read()/1/failure', function(assert) {
 
 	assert(this.server !== null);
 	assert(isFunction(this.server.services.cache.read), true);
 
 	this.server.services.cache.read({
 		domain: 'example.com',
-		path:   '/review/server/cache.json'
+		path:   '/review/server/cache.json?foo=bar'
 	}, (response) => {
 
 		assert(response, {
@@ -171,14 +293,90 @@ describe('Cache.prototype.read()/failure', function(assert) {
 
 });
 
-describe('Cache.prototype.remove()/failure', function(assert) {
+describe('Cache.prototype.remove()/1/failure', function(assert) {
 
 	assert(this.server !== null);
 	assert(isFunction(this.server.services.cache.remove), true);
 
 	this.server.services.cache.remove({
 		domain: 'example.com',
-		path:   '/review/server/cache.json'
+		path:   '/review/server/cache.json?foo=bar'
+	}, (response) => {
+
+		assert(response, {
+			headers: {
+				service: 'cache',
+				event:   'remove'
+			},
+			payload: false
+		});
+
+	});
+
+});
+
+describe('Cache.prototype.info()/2', function(assert) {
+
+	assert(this.server !== null);
+	assert(isFunction(this.server.services.cache.read), true);
+
+	this.server.services.cache.info({
+		domain: 'example.com',
+		path:   '/review/server/cache.json?foo=qux'
+	}, (response) => {
+
+		assert(isObject(response), true);
+		assert(response.headers, {
+			'service': 'cache',
+			'event':   'info'
+		});
+
+		assert(isObject(response.payload), true);
+
+		assert(isObject(response.payload.headers),             true);
+		assert(isNumber(response.payload.headers.size),        true);
+		assert(DATETIME.isDate(response.payload.headers.date), true);
+		assert(DATETIME.isTime(response.payload.headers.time), true);
+
+		assert(isObject(response.payload.payload),             true);
+		assert(isNumber(response.payload.payload.size),        true);
+		assert(DATETIME.isDate(response.payload.payload.date), true);
+		assert(DATETIME.isTime(response.payload.payload.time), true);
+
+	});
+
+});
+
+describe('Cache.prototype.remove()/2/success', function(assert) {
+
+	assert(this.server !== null);
+	assert(isFunction(this.server.services.cache.remove), true);
+
+	this.server.services.cache.remove({
+		domain: 'example.com',
+		path:   '/review/server/cache.json?foo=qux'
+	}, (response) => {
+
+		assert(response, {
+			headers: {
+				service: 'cache',
+				event:   'remove'
+			},
+			payload: true
+		});
+
+	});
+
+});
+
+describe('Cache.prototype.remove()/2/failure', function(assert) {
+
+	assert(this.server !== null);
+	assert(isFunction(this.server.services.cache.remove), true);
+
+	this.server.services.cache.remove({
+		domain: 'example.com',
+		path:   '/review/server/cache.json?foo=qux'
 	}, (response) => {
 
 		assert(response, {
