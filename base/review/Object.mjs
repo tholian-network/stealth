31c0c8cdf72ab47c471309d9181035bd6040c15d
Cookie Engineer <cookiengineer@protonmail.com>
2021-09-26 19:34:02 +0200

    :sparkles: Object.clone() Polyfill

--- base/review/Object.mjs
+++ base/review/Object.mjs
@@ -28,6 +28,40 @@ describe('isObject()', function(assert) {
 
 });
 
+describe('Object.clone()', function(assert) {
+
+	let object1 = { foo: 'bar' };
+	let object2 = new Object({ foo: 'bar' });
+	let object3 = { foo: 'bar', qux: [ 1, 3, 3, 7 ] };
+	let object4 = new Object({ foo: 'bar', qux: [ 1, 3, 3, 7 ] });
+
+	let target3 = {};
+	let target4 = {};
+	let clone1  = Object.clone(null,    object1);
+	let clone2  = Object.clone(null,    object2);
+	let clone3  = Object.clone(target3, object3);
+	let clone4  = Object.clone(target4, object4);
+
+	assert(isObject(clone1), true);
+	assert(isObject(clone2), true);
+	assert(isObject(clone3), true);
+	assert(isObject(clone4), true);
+
+	assert(object1 === clone1, false);
+	assert(object2 === clone2, false);
+	assert(object3 === clone3, false);
+	assert(object4 === clone4, false);
+
+	assert(target3 === clone3, true);
+	assert(target4 === clone4, true);
+
+	assert(object1, clone1);
+	assert(object2, clone2);
+	assert(object3, clone3);
+	assert(object4, clone4);
+
+});
+
 describe('Object.prototype.toString()', function(assert) {
 
 	let object1 = { foo: 'bar' };
--- base/source/Object.mjs
+++ base/source/Object.mjs
@@ -9,5 +9,53 @@ if (typeof Object.isObject !== 'function') {
 
 }
 
+if (typeof Object.clone !== 'function') {
+
+	Object.clone = function(target) {
+
+		target = target instanceof Object ? target : {};
+
+
+		for (let a = 1, al = arguments.length; a < al; a++) {
+
+			let source = arguments[a];
+			if (source) {
+
+				for (let prop in source) {
+
+					if (Object.prototype.hasOwnProperty.call(source, prop) === true) {
+
+						let other_value = source[prop];
+						if (other_value instanceof Array) {
+
+							target[prop] = [];
+							Object.clone(target[prop], source[prop]);
+
+						} else if (other_value instanceof Object) {
+
+							target[prop] = {};
+							Object.clone(target[prop], source[prop]);
+
+						} else {
+
+							target[prop] = source[prop];
+
+						}
+
+					}
+
+				}
+
+			}
+
+		}
+
+
+		return target;
+
+	};
+
+}
+
 export const isObject = Object.isObject;
 
